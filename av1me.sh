5981b77b041c8a3a3974c55c1f24cc3aa5dde816
ockentap <135515708+ockentap@users.noreply.github.com>
2023-12-04 14:00:26 +0100

    Update av1me.sh
    Changed description, quality, output changed from webm to mp4 and encoder changed to libsvtav1 from libaom-av1 due to errors

--- av1me.sh
+++ av1me.sh
@@ -7,9 +7,11 @@
 # Right now, MP4 and mkv are supported input file types.
 # You can specify an video quality for the conversion.
 #
-# Author: Dennis Rohner (@midzer)
-# URL: https://github.com/midzer/av1me
-#
+# (Original)Author: Dennis Rohner (@midzer)
+# (Original)URL: https://github.com/midzer/av1me
+# 
+# Author: Ockentap (@ockentap)
+# URL:https://github.com/ockentap/av1me/
 # License: MIT
 #
 # Version 0.1
@@ -17,7 +19,7 @@
 ###################################################################################################
 
 # Set quality [0-63]
-quality=42
+quality=32
 
 # Determine size of original file
 origFile="$1"
@@ -25,10 +27,11 @@ origFileSize=$(stat -c %s "$origFile")
 
 # Let's start!
 echo "Processing $origFile ..."
-ffmpeg -i $origFile -c:v libaom-av1 -crf $quality -b:v 0 -strict -2 ${origFile%.*}.webm
+# Change libsvtav1 to libaom-av1 (or encoder of your choosing) 
+ffmpeg -i $origFile -c:v libsvtav1 -crf $quality -b:v 0 -strict -2 ${origFile%.*}.mp4
 
 # How did we perform?
-av1edFileSize=$(stat -c %s "${origFile%.*}.webm")
+av1edFileSize=$(stat -c %s "${origFile%.*}.mp4")
 percent=$((100 - av1edFileSize * 100 / origFileSize))
 percent=$( printf '%d' $percent )
 
